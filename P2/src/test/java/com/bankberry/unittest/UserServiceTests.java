c1ba49e187cec6bff9026b6be6fd90739a5ae075
Nicholas Chu <chun@vcu.edu>
2022-10-20 17:58:46 -0400

    outlined unit tests

---
+++ P2/src/test/java/com/bankberry/unittest/UserServiceTests.java
@@ -0,0 +1,24 @@
+package com.bankberry.unittest;
+
+public class UserServiceTests {
+
+    /*
+    -updateChecking
+    -updateCheckingTransaction
+    -updateSaving
+    -updateSavingTransaction
+    -checkingBalances
+    -checkCheckingTransactions
+    -savingBalances
+    -checkSavingTransaction
+    -loginCheck
+    -fundsTransferChecking
+    -fundsTransferSaving
+    -submitLoanAppCheck
+    -emailChange
+    -passChange
+    -checkPendingLoan
+
+
+     */
+}

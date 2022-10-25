7d141b8bd1a2d454ce06079532fd6e6b8865004c
Nicholas Chu <chun@vcu.edu>
2022-10-25 14:16:43 -0400

    made runner class for feature files

---
+++ P2/src/test/java/com/bankberry/runner/Runner.java
@@ -0,0 +1,36 @@
+package com.bankberry.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/com/bankberry/features", glue = "src/test/java/com/bankberry/steps")
+
+public class Runner {
+
+    public static ChromeDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        File chrome = new File("src/test/resources/Drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", chrome.getAbsolutePath());
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @AfterClass
+    public static void teardown() {
+        driver.quit();
+    }
+
+
+
+}

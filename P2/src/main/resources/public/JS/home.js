2ff909dcfd44f3008856205a32c36728b0a0c845
Benjamin Mizell <bmizell22@gmail.com>
2022-10-27 09:08:46 -0500

    Completed Test Plan

--- P2/src/main/resources/public/JS/home.js
+++ P2/src/main/resources/public/JS/home.js
@@ -1,52 +1,49 @@
 async function retrieveData() {
-    const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',});
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
     let baseUrl = "http://localhost:8080";
     let ID = sessionStorage.getItem("loggedInMember");
     let checkingResponse = await fetch(`${baseUrl}/user/${ID}/checkingaccount`);
     if (checkingResponse.status === 200) {
-        
         let checkingAccount = await checkingResponse.json();
         console.log(checkingAccount);
-        document.getElementById("accountBalance").innerText =
-            formatter.format(checkingAccount.balance);
+        document.getElementById("accountBalance").innerText = formatter.format(
+            checkingAccount.balance
+        );
         document.getElementById("accountNumber").innerText =
-        checkingAccount.accountNumber;
+            checkingAccount.accountNumber;
     }
     let savingsResponse = await fetch(`${baseUrl}/user/${ID}/savingsaccount`);
 
-    if(savingsResponse.status===200){
+    if (savingsResponse.status === 200) {
         let savingsAccount = await savingsResponse.json();
 
-        document.getElementById("savingsBalance").innerText = formatter.format(savingsAccount.savingsBalance);
-        document.getElementById("savingsNumber").innerText = savingsAccount.savingsAccountNumber;
-
-
+        document.getElementById("savingsBalance").innerText = formatter.format(
+            savingsAccount.savingsBalance
+        );
+        document.getElementById("savingsNumber").innerText =
+            savingsAccount.savingsAccountNumber;
     }
 
     let loanAppResponse = await fetch(`${baseUrl}/user/${ID}/loanapps`);
 
-    if(loanAppResponse.status===200){
-
+    if (loanAppResponse.status === 200) {
         let loanApp = await loanAppResponse.json();
         console.log(loanApp);
-        loanApp.forEach(app =>{updateTable(app)
-            
+        loanApp.forEach((app) => {
+            updateTable(app);
         });
-       
-
     }
-
-    
-
-
 }
 
-function updateTable(loanApp){
-    const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',});
+//TODO: Add a similar function to this one in savingsAccount.js and checkingAccount.js
+function updateTable(loanApp) {
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
     let newAppBody = document.getElementById("loanTableBody");
     let newAppRow = document.createElement("tr");
 
@@ -62,9 +59,11 @@ function updateTable(loanApp){
     let termInMonthsTD = document.createElement("td");
     termInMonthsTD.innerHTML = loanApp.termInMonths + " Months";
 
-    newAppRow.append(loanAppIdTD, loanStatusTD, loanAppAmountTD, termInMonthsTD);
+    newAppRow.append(
+        loanAppIdTD,
+        loanStatusTD,
+        loanAppAmountTD,
+        termInMonthsTD
+    );
     newAppBody.append(newAppRow);
-
-
-    
 }
